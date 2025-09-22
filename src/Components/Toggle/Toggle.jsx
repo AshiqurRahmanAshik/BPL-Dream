5e4fa0fc7607630766003a3c53d4fc4661630797
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-09-22 10:22:35 +0600

    Toggle Highlight Feature Added

--- src/Components/Toggle/Toggle.jsx
+++ src/Components/Toggle/Toggle.jsx
@@ -4,8 +4,34 @@ import AvailablePlayers from "./../AvailablePlayers/AvailablePlayers";
 
 const Toggle = () => {
   const [toggle, setToggle] = useState(true);
+
   return (
     <div>
+      <div className="flex justify-between w-11/12 mx-auto">
+        <p className="text-xl font-bold">Available Players</p>
+        <div className="space-x-0.5">
+          <button
+            onClick={() => {
+              setToggle(true);
+            }}
+            className={`border px-2 py-1 rounded font-semibold ${
+              toggle === true ? "bg-yellow-200" : ""
+            }`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => {
+              setToggle(false);
+            }}
+             className={`border px-2 py-1 rounded font-semibold ${
+              toggle === false ? "bg-yellow-200" : ""
+            }`}
+          >
+            Selected (<span>0</span>)
+          </button>
+        </div>
+      </div>
       {toggle === true ? (
         <AvailablePlayers></AvailablePlayers>
       ) : (
